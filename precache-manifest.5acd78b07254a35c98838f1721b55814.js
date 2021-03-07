e4b6ae06d8641821676e9872d3c49efff043968e
aybarsaltinisik <aybars.altinisik@gmail.com>
2021-03-07 19:46:06 +0300

    Updates

---
+++ precache-manifest.5acd78b07254a35c98838f1721b55814.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd71d1c1d35e6d8f7ee091a264151ab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a65983498766f8609beb",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65983498766f8609beb",
+    "url": "/static/js/main.c865ded0.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "866fa21576f2fe83da37b686c3aaf2ca",
+    "url": "/static/media/picture.866fa215.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
